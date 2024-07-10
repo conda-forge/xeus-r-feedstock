1f50684c4324e58f2bb0f65ae6a003b2dbc208bf
Sylvain Corlay <sylvain.corlay@gmail.com>
2024-07-10 22:38:50 +0200

    Build xeus-r on Windows

---
+++ recipe/bld.bat
@@ -0,0 +1,18 @@
+REM Specifying the XEUS_PYTHONHOME_RELPATH to the general prefix.
+
+cmake -G "NMake Makefiles" ^
+  -D CMAKE_BUILD_TYPE=Release ^
+  -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  %SRC_DIR%
+if errorlevel 1 exit 1
+
+nmake
+if errorlevel 1 exit 1
+
+nmake install
+if errorlevel 1 exit 1
+
+REM Copying kernelspec to the general prefix for Jupyter to pick it up.
+
+md %PREFIX%\share\jupyter\kernels\xr
+xcopy %LIBRARY_PREFIX%\share\jupyter\kernels\xr %PREFIX%\share\jupyter\kernels\xr /F /Y
