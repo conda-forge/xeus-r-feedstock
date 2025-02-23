aa591a71883ed81a82820d0c0ef34763bcb06e63
Isabel Paredes <isabel.paredes@quantstack.net>
2025-02-23 22:56:45 +0100

    Rename variable

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,7 +23,7 @@ cmake ${CMAKE_ARGS} \
       -DCMAKE_INSTALL_PREFIX=$PREFIX  \
       -DCMAKE_PREFIX_PATH=$PREFIX     \
       -DCMAKE_INSTALL_LIBDIR=lib      \
-      -DINSTALL_HERA=${INSTALL_HERA}  \
+      -DXEUS_R_INSTALL_HERA=${INSTALL_HERA}  \
       $SRC_DIR
 
 make install
